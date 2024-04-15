b52f17ef1d9078d73375bc834c664f34442f59bd
Julio Cesar <js.cesar42@gmail.com>
2024-04-15 19:41:08 -0300

    Initial Commit

---
+++ StarbucksLoginSizeClassesExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StarbucksLoginSizeClassesExample
+//
+//  Created by Julio Cesar Pereira on 15/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ StarbucksLoginSizeClassesExampleUITests/StarbucksLoginSizeClassesExampleUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  StarbucksLoginSizeClassesExampleUITestsLaunchTests.swift
+//  StarbucksLoginSizeClassesExampleUITests
+//
+//  Created by Julio Cesar Pereira on 15/04/24.
+//
+
+import XCTest
+
+final class StarbucksLoginSizeClassesExampleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
